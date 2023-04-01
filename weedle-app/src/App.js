2e50a0734d486fcb4427bdcbd7d86b1be6e56927
WilliamMorano <wfm224@lehigh.edu>
2023-04-01 14:45:41 -0400

    added

--- weedle-app/src/App.js
+++ weedle-app/src/App.js
@@ -1,33 +1,19 @@
 import logo from './logo.svg';
 import './App.css';
-import { Routes, Route, Link } from 'react-router-dom';
+import { Routes, Route, BrowserRouter } from 'react-router-dom';
 import Home from './Home';
 import LeaderBoard from './LeaderBoard';
 import Radar from './Radar';
-import React, { useRef } from 'react';
 
 function App() {
   return (
-    <div>
-      <nav>
-        <ul>
-          <li>
-            <Link to="/">Home</Link>
-          </li>
-          <li>
-            <Link to="/about">About</Link>
-          </li>
-          <li>
-            <Link to="/contact">Contact</Link>
-          </li>
-        </ul>
-      </nav>
+    <BrowserRouter>
       <Routes>
-        <Route exact path="/" component={Home} />
-        <Route path="/leaderboard" component={LeaderBoard} />
-        <Route path="/radar" component={Radar} />
+        <Route path="/" element={<Home />} />
+        <Route path="/radar" element={<Radar />} />
+        <Route path="/leaderboard" element={<LeaderBoard />} />
       </Routes>
-    </div>
+    </BrowserRouter>
   );
 }
 
--- weedle-app/src/index.js
+++ weedle-app/src/index.js
@@ -28,6 +28,6 @@ const analytics = getAnalytics(app);
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <React.StrictMode>
-      <App />
+    <App />
   </React.StrictMode>
 );
