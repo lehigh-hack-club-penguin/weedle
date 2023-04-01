b723f951712eca8a5cae05650f4fda2bd5c1ec9d
WilliamMorano <wfm224@lehigh.edu>
2023-04-01 13:51:53 -0400

    new branch

--- weedle-app/src/App.js
+++ weedle-app/src/App.js
@@ -1,23 +1,32 @@
 import logo from './logo.svg';
 import './App.css';
+import { Routes, Route, Link } from 'react-router-dom';
+import Home from './Home';
+import LeaderBoard from './LeaderBoard';
+import Radar from './Radar';
+import React, { useRef } from 'react';
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div>
+      <nav>
+        <ul>
+          <li>
+            <Link to="/">Home</Link>
+          </li>
+          <li>
+            <Link to="/about">About</Link>
+          </li>
+          <li>
+            <Link to="/contact">Contact</Link>
+          </li>
+        </ul>
+      </nav>
+      <Routes>
+        <Route exact path="/" component={Home} />
+        <Route path="/leaderboard" component={LeaderBoard} />
+        <Route path="/radar" component={Radar} />
+      </Routes>
     </div>
   );
 }
---
+++ weedle-app/src/Radar.jsx
@@ -0,0 +1,9 @@
+import React from 'react'
+
+const Radar = () => {
+  return (
+    <div>Radar</div>
+  )
+}
+
+export default Radar
