3e9dab6c87f83e5da9bbc4225afa265460dba60a
WilliamMorano <wfm224@lehigh.edu>
2023-04-01 15:05:29 -0400

    added nav

--- weedle-app/src/App.js
+++ weedle-app/src/App.js
@@ -1,14 +1,20 @@
 import './App.css';
 import React from 'react';
 import { BrowserRouter, Route, Routes } from "react-router-dom";
-import Login from './components/Login';
+import Home from './components/Home';
+import LeaderBoard from './components/LeaderBoard';
+import Radar from './components/Radar';
+import Navbar from './components/Navbar';
 
 function App() {
   return (
     <BrowserRouter>
     <div className="App">
+    <Navbar />
     <Routes>
-        <Route path="/" element={<Login />} />
+        <Route path="/" element={<Home />} />
+        <Route path="/radar" element={<Radar />}></Route>
+        <Route path="/leaderboard" element={<LeaderBoard />}></Route>
     </Routes>
     </div>
     </BrowserRouter>
