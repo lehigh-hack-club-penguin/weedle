8af168d1cc69f9866b56717410e4bdb3749f11c9
WilliamMorano <wfm224@lehigh.edu>
2023-04-01 16:11:27 -0400

    fixed bug

--- weedle-app/src/index.js
+++ weedle-app/src/index.js
@@ -7,7 +7,7 @@ import App from './App';
 import { initializeApp } from "firebase/app";
 import { getDatabase } from "firebase/database";
 import { getAnalytics } from "firebase/analytics";
-import { getDatabase } from "firebase/database";
+
 // TODO: Add SDKs for Firebase products that you want to use
 // https://firebase.google.com/docs/web/setup#available-libraries
 
