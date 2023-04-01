2c0c2ae42a9d60a6b51a57d21dc9f55435d3c10f
WilliamMorano <wfm224@lehigh.edu>
2023-04-01 15:52:10 -0400

    added stuff

--- weedle-app/src/components/Radar.js
+++ weedle-app/src/components/Radar.js
@@ -2,9 +2,11 @@ import React from 'react'
 import './styles/RadarStyles.css'
 export default function Radar() {
     return (
-        <div>
+        <div className='container'>
             <div className='plant-list'>
-
+                <div className='invasive-title'>
+                    <div className='center-text'>Invasive Plants</div>
+                </div>
             </div>
             <div className='plant-map'>
 
