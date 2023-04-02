4a0d5133b348a46bca9b4b5ef08925b3615bb60a
Hana <and224@lehigh.edu>
2023-04-02 00:33:20 -0400

    points increased

--- weedle-app/src/components/Upload.js
+++ weedle-app/src/components/Upload.js
@@ -1,6 +1,6 @@
 import React, { useState } from 'react';
 import Modal from 'react-bootstrap/Modal';
-import { ref, onValue, set } from "firebase/database";
+import { ref, onValue, set, child, push, update } from "firebase/database";
 import Form from 'react-bootstrap/Form';
 import Button from 'react-bootstrap/Button';
 // import css
@@ -9,6 +9,9 @@ import './styles/Upload.css';
 export default function Upload(props) {
     const [file, setFile] = useState(null);
     const [attemptedUpload, setAttemptedUpload] = useState(false);
+    const [showPointsNotification, setShowPointsNotification] = useState(false);
+    const userID = localStorage.getItem('userID');
+    const [points, setPoints] = useState(0);
 
     const handleFileChange = (event) => {
         setFile(event.target.files[0]);
@@ -18,8 +21,31 @@ export default function Upload(props) {
         if (file !== null && file !== undefined) {
             console.log(file);
             // TODO: UPLOAD FILE TO FIREBASE
-            // TODO: 
-            props.handleCloseUpload();
+            // UPDATE POINTS FOR USER
+            // get user's current points
+            // we have table users with userID as key
+            // each user has a points field
+            console.log('user id: ' + userID);
+            const query = ref(props.db, 'users/' + userID);
+            onValue(query, (user) => {
+                const data = user.val();
+                // console.log(data);
+                // console.log(data.points);
+                setPoints(data.points);
+            });
+            // add 10 points to user's points
+            const newPoints = points + 10;
+            update(ref(props.db, '/users/' + userID), {
+                points: newPoints
+            });
+            // show points notification
+            setShowPointsNotification(true);
+            setTimeout(() => {
+                    setShowPointsNotification(false);
+                }, 3000);
+            // close modal
+            handleCloseUpload();
+            
         } else {
             setAttemptedUpload(true);
         }
@@ -59,6 +85,11 @@ export default function Upload(props) {
                     </Button>
                 </Modal.Footer>
             </Modal>
+            {showPointsNotification && (
+                <div className="points-notification">
+                    +10 points!
+                </div>
+            )}
         </>
     );
 }
