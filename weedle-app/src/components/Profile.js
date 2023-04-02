9a1219d96c32789b71462d41f6c3d50a0a70c33a
Adrian Ross <amr424@lehigh.edu>
2023-04-02 08:47:26 -0400

    profile clip works

--- weedle-app/src/components/Profile.js
+++ weedle-app/src/components/Profile.js
@@ -3,9 +3,30 @@ import Modal from 'react-bootstrap/Modal';
 import { ref, onValue, set, child, push, update, get } from "firebase/database";
 import Form from 'react-bootstrap/Form';
 import Button from 'react-bootstrap/Button';
+import Row from 'react-bootstrap/Row';
+import Col from 'react-bootstrap/Col';
+import Container from 'react-bootstrap/Container';
+import logo from '../logo.svg'
+import './styles/Profile.css'
 
 export default function Profile(props) {
-    const userID = localStorage.getItem('userID'); 
+    const userID = localStorage.getItem('userID');
+    const [user, setUser] = useState("");
+    const [points, setPoints] = useState(0);
+
+    // LOAD DATA FOR OPTIONS
+    useEffect(() => {
+        if (userID !== null) {
+            const query = ref(props.db, 'users/' + userID);
+            onValue(query, (user) => {
+                const data = user.val();
+                setUser(user.val())
+                console.log(user.val());
+                // console.log(data.points);
+                setPoints(data.points);
+            });
+        }
+    }, []);
 
     const handleCloseProfile = () => {
         props.handleCloseProfile();
@@ -15,9 +36,29 @@ export default function Profile(props) {
         <>
             <Modal show={props.showProfile} onHide={handleCloseProfile} centered >
                 <Modal.Header closeButton>
-                    <Modal.Title>Let's Weedle!</Modal.Title>
                 </Modal.Header>
-                <Modal.Body>
+                <Modal.Body className="show-grid">
+                    <Container fluid>
+                    <Row>
+                        <Col xs={6} md={4}>
+                            <div >
+                                <img src={user.profile_url} alt="Avatar" style={{ width: '100px', height: '100px', borderRadius: '50%' }} />
+                            </div>
+                        </Col>
+                        <Col xs={12} md={8}>
+                            <br></br>
+                            Welcome back <strong>{user.username}</strong>!
+                            <br></br>
+                            <br></br>
+                            You have <strong>{points}</strong> points.
+                        </Col>
+                    </Row>
+                    </Container>
+                    <br></br>
+                    <br></br>
+                    <br></br>
+                </Modal.Body>
+                {/* <Modal.Body>
                 <br />
                 <p>
                     <strong>Regional Rarity: </strong> {}
@@ -25,8 +66,11 @@ export default function Profile(props) {
                 <p>
                     <strong>Points: </strong> {}
                 </p>
-                </Modal.Body>
+                </Modal.Body> */}
                 <Modal.Footer>
+                <div style={{ position: 'relative',left:'50px' }}>
+                        <img src={logo} alt="Weedle Logo" style={{ width: '10%', height: 'auto' }} />
+                </div>
                 </Modal.Footer>
             </Modal>
         </>
